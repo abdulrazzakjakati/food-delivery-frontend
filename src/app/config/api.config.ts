46d5be097d1cb1203b2971349f73c95b5d5f4b80
abdulrazzak jakati <abdulrazzak.jakati@gmail.com>
2026-04-21 21:51:08 +0530

    v1 update configURL

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,9 @@
-export const API_URL_RL = 'http://localhost:9091';
-export const API_URL_ORDER = 'http://localhost:9093';
-export const API_URL_FC = 'http://localhost:9092';
-export const API_URL_UD = 'http://localhost:9094';
+// export const API_URL_RL = 'http://localhost:9091';
+// export const API_URL_ORDER = 'http://localhost:9093';
+// export const API_URL_FC = 'http://localhost:9092';
+// export const API_URL_UD = 'http://localhost:9094';
+
+export const API_URL_RL = '/api/restaurant';
+export const API_URL_ORDER = '/api/order';
+export const API_URL_FC = '/api/food';
+export const API_URL_UD = '/api/user';
